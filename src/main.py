ffecee80a9cee5a765675ec560ab641b110407c2
Karinohana <Karinohana95@gmail.com>
2022-05-17 02:26:19 +0000

    Star-Wars-Python

--- src/main.py
+++ src/main.py
@@ -9,7 +9,8 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
-#from models import Person
+from models import People
+from models import Planets
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -32,6 +33,104 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
+
+    response_body = {
+        "msg": "Hello, this is your GET /user response "
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/user/favorites', methods=['GET'])
+def handle_user_favorites():
+
+    response_body = {
+        "msg": "Hello, this is your GET /user response "
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/people', methods=['GET'])
+def handle_people():
+    table = People.query.all()
+    data = list(map(lambda x: x.serialize(),table))
+
+    return jsonify(data), 200
+
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def handle_people_id():
+
+    response_body = {
+        "msg": "Hello, this is your GET /user response "
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/planets', methods=['GET'])
+def handle_planets():
+    table = Planets.query.all()
+    data = list(map(lambda x: x.serialize(),table))
+
+    return jsonify(data), 200
+
+@app.route('/planets', methods=['POST'])
+def handle_post_planet():
+    payload = request.get_json()
+    info = Planets(name = payload["name"],climate = payload["climate"], terrain = payload["terrain"])
+    db.session.add(info)
+    db.session.commit()
+
+    return "success", 200
+
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def handle_planets_id():
+
+    response_body = {
+        "msg": "Hello, this is your GET /user response "
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def handle_favs_planet():
+
+    response_body = {
+        "msg": "Hello, this is your GET /user response "
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def handle_favs_people():
+
+    response_body = {
+        "msg": "Hello, this is your GET /user response "
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/people', methods=['POST'])
+def handle_post_people():
+    payload = request.get_json()
+    info = People(name = payload["name"],gender = payload["gender"], eye_color = payload["eye_color"])
+    db.session.add(info)
+    db.session.commit()
+
+
+    return jsonify("success"), 200
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def handle_delete_planet():
+
+    response_body = {
+        "msg": "Hello, this is your GET /user response "
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/favorite/people/<int:people_id> ', methods=['DELETE'])
+def handle_delete_people():
 
     response_body = {
         "msg": "Hello, this is your GET /user response "
--- src/models.py
+++ src/models.py
@@ -17,3 +17,37 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    climate = db.Column(db.String(80), unique=False, nullable=False)
+    terrain = db.Column(db.String(80), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.id
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            # do not serialize the password, its a security breach
+        }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    gender = db.Column(db.String(80), unique=False, nullable=False)
+    hair_color = db.Column(db.String(80), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<People %r>' % self.id
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
+            # do not serialize the password, its a security breach
+        }
